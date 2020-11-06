9c5493632178be05f6d3e7b24489081d29e7b40e
Dmitry <dimasharonov95@yandex.ru>
2020-11-06 17:36:37 +0300

    Исправлены расчёты физической модели

--- solar_model.py
+++ solar_model.py
@@ -19,9 +19,18 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
-        pass  # FIXME: Взаимодействие объектов
+        r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
+        r = max(r, body.R)
+
+        # силя гравитации
+        f = gravitational_constant * body.m * obj.m / r ** 2
+
+        # проекции
+        cosa = (obj.x - body.x) / r
+        sina = (obj.y - body.y) / r
+        body.Fx += f * cosa
+        body.Fy += f * sina
+
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
@@ -30,12 +39,17 @@ def move_space_object(body, dt):
 
     **body** — тело, которое нужно переместить.
     """
-    old = body.x  # FIXME: Вывести формулы для ускорения, скоростей и координат
-    ax = body.Fx/body.m
-    body.x += 24
-    ay = body.Fy*body.m
-    body.y = 42
-    body.Vy += 4*dt
+    # ускорения
+    accelerate_x = body.Fx / body.m
+    accelerate_y = body.Fy / body.m
+
+    # перемещения
+    body.x += body.Vx * dt + accelerate_x * dt * dt / 2
+    body.y += body.Vy * dt + accelerate_y * dt * dt / 2
+
+    # скорости
+    body.Vx += accelerate_x * dt
+    body.Vy += accelerate_y * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
